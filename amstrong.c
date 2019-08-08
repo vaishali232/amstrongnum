962b2e42beae8761328212529879ce08150f3032
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-08-08 12:49:36 +0530

    Create amstrong.c

---
+++ amstrong.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+unsigned long long int n,tempnum,ams=0,rem;
+scanf("%llu",%n);
+tempnum=n;
+while(n>0)
+{
+rem=n%10;
+ams+=rem*rem*rem;
+n/=10;
+}
+if(ams==tempnum)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+
+}
